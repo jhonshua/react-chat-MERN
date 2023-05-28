2a4cb04878c6061757a00761ac120ff51b64e3c9
jhonshua <julio.llinas@gmail.com>
2023-05-28 17:57:22 -0400

    fix update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4000';
+  axios.defaults.baseURL = 'https://api-chat-mern-production.up.railway.app/';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
